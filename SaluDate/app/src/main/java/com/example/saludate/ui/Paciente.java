9a0ed7adffd0a209fd606a6240a6d29dff49f3e9
Waxiest15 <waxiest15@gmail.com>
2022-11-16 21:43:54 -0600

    Actualizacion 16/11/2022
    Agregado de la pantalla de Paciente (Sin imagenes relacioandas)

---
+++ SaluDate/app/src/main/java/com/example/saludate/ui/Paciente.java
@@ -0,0 +1,34 @@
+package com.example.saludate.ui;
+
+import androidx.lifecycle.ViewModelProvider;
+import android.os.Bundle;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import com.example.saludate.R;
+
+public class Paciente extends Fragment {
+
+    public static Paciente newInstance() {
+        return new Paciente();
+    }
+
+    private PacienteViewModel mViewModel;
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_paciente, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(PacienteViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ SaluDate/app/src/main/java/com/example/saludate/ui/PacienteViewModel.java
@@ -0,0 +1,7 @@
+package com.example.saludate.ui;
+
+import androidx.lifecycle.ViewModel;
+
+public class PacienteViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
