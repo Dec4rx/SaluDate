29d18cdae67fa1534898802d9d2148b34c54e6a1
Dec4rx <decarwar@gmail.com>
2022-11-18 01:44:02 -0600

    TODAS LAS ACTIVITIES
    FINAAALMEEENTEEEE
    AAAAAAAAAAAAAAHHHHHHHHH

---
+++ SaluDate/app/src/main/java/com/example/saludate/MedicalTest_e.java
@@ -0,0 +1,14 @@
+package com.example.saludate;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MedicalTest_e extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_medical_test_e);
+    }
+}
